9bd5882b6de63e28f3dd34087731f86e99e0d8ec
nikihater <88324592+nikihater@users.noreply.github.com>
2026-05-26 21:11:23 +0300

    modified:   api.py

--- api.py
+++ api.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://186.246.14.209:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
